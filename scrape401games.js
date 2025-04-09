7580d0542742b16ec0939ec9f24bc1a84778b0c5
VisaganS <visagan.shan@gmail.com>
2025-04-09 13:47:38 -0400

    Updated script to use Puppeteer for dynamic web scraping

--- scrape401games.js
+++ scrape401games.js
@@ -1,23 +1,46 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const puppeteer = require('puppeteer')
+
 
 async function scrape401games(cardName) {
-
-    // format card name and retrieve search results
-    // const searchTerm = cardName.trim().replace(/\s+/g, '+');
-    const searchTerm = 'Bystial Lubellion';
+    // format card name and URL
+    const searchTerm = cardName.trim().replace(/\s+/g, '+');
     const searchURL = `https://store.401games.ca/pages/search-results?q=${searchTerm}`
-
+    
+    // launch headless browser 
+    let browser;
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
     try {
-        const { data } = await axios.get(searchUrl);
-        const $ = cheerio.load(data); //parse HTML
+        // set page configs and navigate to the search URL
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36');
+        page.setDefaultNavigationTimeout(2 * 60 * 1000);
+        await page.goto(searchURL, { waitUntil: 'networkidle2'});
 
-        const results = [];
-        console.log(results);
-        
+        // evaluate page and extract product details
+        const products = await page.evaluate(() => {
+            // get all products inside product grid
+            const productCards = Array.from(document.querySelectorAll('.products-grid .product-card')); // use Array.from() to convert nodelist to array for .map() operations
+
+            return productCards.forEach(card => {
+                // extract product name
+                const titleEl = card.querySelector('.product-card-items-wrapper .info-container .title-container .title-wrapper .title');
+                const name = titleEl ? titleEl.textContent.trim() : null;
+                
+                // extract product price
+                const priceEl = product.querySelector('.product-card-items-wrapper .info-container .price-container .price')
+                let price = priceEl ? priceEl.textContent.trim() : null;
+                //remove CAD from price text
+
+                return { name, price };
+            }); 
+        });
+        console.log('Products: ', products);
+        await browser.close();
+        return products;
     } catch(err) {
         console.error('Error scraping 401 Games: ', err.message);
         return [];
     }
 }
+
+scrape401games('Bystial Lubellion')
